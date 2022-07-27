c49f1c5e134bfc1708a02f00fb228354f62b6648
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-26 20:41:08 -0700

    add validation for password match and css for unvalidation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const password = document.querySelector("#password")
 const confirmPassword = document.querySelector("#confirm_password")
+const submitBtn = document.querySelector("#submitButton")
+const input = document.querySelectorAll(".input")
 
 confirmPassword.addEventListener('input', () => {
     console.log(password.value, confirmPassword.value)
@@ -10,3 +12,20 @@ confirmPassword.addEventListener('input', () => {
         password.setCustomValidity("")
     }
 })
+
+submitBtn.addEventListener('click', () => {
+    input.forEach(event => {
+        if(event.reportValidity() === false) {
+            event.style.border = "#e40c2b 3px solid"
+        } else {
+            event.style.border = "#000000 1px solid"
+        }
+    })
+})
+
+input.forEach(event => {
+    event.addEventListener('input', () => {
+        event.style.border = "#000000 1px solid"
+    })
+})
+
