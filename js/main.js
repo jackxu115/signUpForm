3abbcff54671e4ddad08142cf7f9b7cf9f9cef12
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-26 22:02:33 -0700

    fix js after validation

--- js/main.js
+++ js/main.js
@@ -3,29 +3,42 @@ const confirmPassword = document.querySelector("#confirm_password")
 const submitBtn = document.querySelector("#submitButton")
 const input = document.querySelectorAll(".input")
 
-confirmPassword.addEventListener('input', () => {
+// password match
+const passwordMatch = () => {
     console.log(password.value, confirmPassword.value)
     if (password.value !== confirmPassword.value) {
-        password.setCustomValidity("Password do not match")
-        confirmPassword.reportValidity()
+        confirmPassword.setCustomValidity("Password do not match")
+        // password.style.border = "#e40c3b 3px solid"
     } else {
-        password.setCustomValidity("")
+        confirmPassword.setCustomValidity("")
     }
-})
+}
 
+// check form validation after click button to submit the form
 submitBtn.addEventListener('click', () => {
     input.forEach(event => {
-        if(event.reportValidity() === false) {
+        console.log(event.reportValidity())
+        if (event.reportValidity() === false) {
             event.style.border = "#e40c2b 3px solid"
         } else {
             event.style.border = "#000000 1px solid"
         }
     })
+    changBorderColor()
+    passwordMatch()
 })
 
-input.forEach(event => {
-    event.addEventListener('input', () => {
-        event.style.border = "#000000 1px solid"
+
+// change border color of input when focusin
+const changBorderColor = () => {
+    input.forEach(event => {
+        event.addEventListener('focus', () => {
+            event.style.border = "#000000 3px solid"
+        })
     })
-})
-
+    input.forEach(event => {
+        event.addEventListener('keyup', () => {
+            event.style.border = "#000000 1px solid"
+        })
+    })
+}
